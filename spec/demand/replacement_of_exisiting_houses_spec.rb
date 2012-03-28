2b239cae0bf57b7306d73d0412a58aa0fdb15dcb
Chael Kruip <chael.kruip@quintel.com>
2012-03-28 14:55:20 +0200

    Added some tests to the remplacement_of_exisiting_houses_spec.rb

--- spec/demand/replacement_of_exisiting_houses_spec.rb
+++ spec/demand/replacement_of_exisiting_houses_spec.rb
@@ -7,7 +7,15 @@ describe "Replacement of existing houses" do
 
   before(:all) do
     @scenario = Scenario.new
+
+    # store result of insulation savings before exisiting houses is changed
+    @scenario.move_slider 337, 6.0
+    savings_new_houses_before = @scenario.result("heating_savings_insulation_new_households_energetic")
+    savings_old_houses_before = @scenario.result("extra_insulation_savings_households_energetic")
+    
+    # move slider 1
     @scenario.move_slider 1, 2.5
+    
   end
 
   it "should not increase primary demand" do
@@ -23,8 +31,15 @@ describe "Replacement of existing houses" do
   end
 
   it "should decrease the heat demand for houses between the min and max values" do
-    @scenario.result("heat_demand_including_electric_heating_in_use_of_final_demand_in_households").decrease.should be > 0
+    @scenario.result("heat_demand_including_electric_heating_in_use_of_final_demand_in_households").decrease.should be >  VALUE_IF_NO_HOUSES_REPLACED
+    @scenario.result("heat_demand_including_electric_heating_in_use_of_final_demand_in_households").decrease.should be <  VALUE_IF_ALL_HOUSES_REPLACED
   end
-
+  
+  it "should increase the heat savings for new houses (if insulation of new houses > 0)" do
+    @scenario.result("heating_savings_insulation_new_households_energetic") > savings_new_houses_before   
+  end
+  it "should decrease the heat savings for old houses (if insulation of old houses > 0)" do
+    @scenario.result("extra_insulation_savings_households_energetic") < savings_old_houses_before   
+  end
   
 end
