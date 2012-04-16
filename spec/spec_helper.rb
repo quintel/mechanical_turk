fe7f6a680d156e728147664436b0c2ecafb37336
Dennis Schoenmakers <dennis.schoenmakers@quintel.com>
2012-04-16 13:13:30 +0200

    improved on spec_helper error messages for increase, decrease and not_change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,30 @@
 Dir[File.expand_path("../../lib/*.rb", __FILE__)].each {|f| require f}
 
 RSpec::Matchers.define :increase do
-  match do |actual|
-    actual.increase > 0
+  match{ |actual| actual.increase > 0 }
+  failure_message_for_should do |actual|
+    if actual.increase == 0 
+      "expected an increase, but actually it stayed the same: #{actual.future}"
+    else
+      "expected an increase, but actually got a decrease of #{actual.decrease}"
+    end
   end
 end
 
 RSpec::Matchers.define :decrease do
-  match do |actual|
-    actual.decrease > 0
+  match { |actual| actual.decrease > 0 }
+  failure_message_for_should do |actual|
+    if actual.decrease == 0 
+      "expected a decrease, but actually it stayed the same: #{actual.future}"
+    else
+      "expected a decrease, but actually got a increase of #{actual.increase}"
+    end
   end
 end
 
 RSpec::Matchers.define :not_change do
-  match do |actual|
-    actual.decrease == 0
+  match { |actual| actual.decrease == 0 }
+  failure_message_for_should do |actual|
+    "I expected the value not to change, but actually it changed with #{actual.increase}"
   end
 end
