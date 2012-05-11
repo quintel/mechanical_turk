85bebd50e2464dd9f642e567b446a73a4101bb83
Chris <chris.laumans@quintel.com>
2012-05-11 15:36:32 +0200

    merit order mechanical turk

--- spec/merit_order/merit_order_spec.rb
+++ spec/merit_order/merit_order_spec.rb
@@ -9,31 +9,54 @@ describe "merit order module" do
     @scenario = Scenario.new(country: "nl", end_year: 2050)
   end
 
-  context "when power plant sliders have not been moved" do
-
-    it "should set full load hours to zero if power plant sliders moved to zero" do
-    #  @scenario.set_input 257, 0 #%
-    #  @scenario.co2.should increase
-
-      @scenario.set_input 257, 0 # number of gas ccgt
-      @scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table.should decrease
-
-    #  @scenario.set_input 259, 0 # number of nuclear plants
-    #  @scenario.result("merit_order_nuclear_iii_full_load_hours_in_merit_order_table").value.should be == 0
-      
-    #  @scenario.set_input 250, 0 # number of coal pwd
-    #  @scenario.result("merit_order_coal_pwd_full_load_hours_in_merit_order_table").value.should be == 0
+  context "for the start scenario" do
+    
+    it "the full load hours of the cheapest five power plants should be greater than zero and less than 8760" do
+      @scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table.value.should be_within(4379).of(4380)
+      @scenario.merit_order_coal_igcc_full_load_hours_in_merit_order_table.value.should be_within(4379).of(4380)
+      @scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table.value.should be_within(4379).of(4380)
+      @scenario.merit_order_coal_chp_full_load_hours_in_merit_order_table.value.should be_within(4379).of(4380)
+      @scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table.value.should be_within(4379).of(4380)
     end
-
   end
 
-#  context "when natural gas becomes very cheap, full load hours of power plants should increase" do
-#
-#    it "should lower gas full load hours if natural gas price increases" do
-#      @scenario.set_input , -100  # % price decrease natural gas
-#      @scenario.result("merit_order_gas_ccgt_full_load_hours_in_merit_order_table").should increase
-#      @scenario.result("merit_order_gas_conv_full_load_hours_in_merit_order_table").should increase
-#    end
-#  end
+  context "when removing power plants" do
+
+    it "the full load hours of a power plant should become zero if the number of units becomes zero" do
+      @scenario.set_input 257, 0 # number of gas ccgt
+      @scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table.value.should be == 0.0
+
+      @scenario.set_input 259, 0 # number of nuclear plants
+      @scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table.value.should be == 0.0
+      
+      @scenario.set_input 250, 0 # number of coal pwd
+      @scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table.value.should be == 0.0
+    end
+  end
+
+  context "when adding wind power" do
+
+    it "should lower full load hours of central production power plants" do
+      @scenario.set_input 265, 4000 # number of wind offshore turbines
+      @scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table.should decrease
+      @scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table.should decrease
+      @scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table.should decrease
+    end
+  end
+  
+  context "when fuel costs change" do
+
+    it "should increase full load hours of gas power plants when natural gas price decreases" do
+      @scenario.set_input 58, -50 # % decrease of natural gas costs
+      @scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table.should increase
+    #  @scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table.should decrease
+    #  @scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table.should decrease
+    end
+    
+    it "should decrease full load hours of coal power plants when coal price increases" do
+        @scenario.set_input 57, 300 # % decrease of coal costs
+        @scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table.should decrease
+    end
+  end
 
 end
