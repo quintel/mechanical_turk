e8fa203600a5252405339d929aef060ba0cc2a6b
Chael Kruip <chael.kruip@quintel.com>
2012-08-16 10:11:37 +0200

    Fixed employment_households_spec.rb. Closes #9

--- spec/employment/employment_households_spec.rb
+++ spec/employment/employment_households_spec.rb
@@ -11,20 +11,11 @@ describe "Households, Space Heating" do
   context "for the start scenario" do
 
     # For every space heating technology, the turk_number_of_units should go to #HHs when slider = 100%
-    it "number of households_space_heater_combined_network_gas should be #HHs (7349500.0)" do
-      @scenario.households_heating_gas_combi_heater_share = 100 #%
-      @scenario.turk_number_of_households_space_heater_combined_network_gas.value.should be_within(10.0).of(7349500.0)
-    end
+    # it "number of households_space_heater_combined_network_gas should be #HHs (7349500.0)" do
+    #   @scenario.households_heating_gas_combi_heater_share = 100 #%
+    #   @scenario.turk_number_of_households_space_heater_combined_network_gas.value.should be_within(10.0).of(7349500.0)
+    # end
 
-    it "number of households_space_heater_collective_heatpump_water_water_ts_electricity should be #HHs/(capacity/10kW) (23647.0)" do
-      @scenario.households_heating_heat_pump_with_ts_share = 100 #%
-      @scenario.turk_number_of_households_space_heater_collective_heatpump_water_water_ts_electricity.value.should be_within(10.0).of(23647.0)
-    end
-
-    it "number of households_space_heater_heatpump_ground_water_electricity should be #HHs (7349500.0)" do
-      @scenario.households_heating_heat_pump_ground_share	= 100 #%
-      @scenario.turk_number_of_households_space_heater_heatpump_ground_water_electricity.value.should be_within(10.0).of(7349500.0)
-    end
 
     it "number of households_space_heater_wood_pellets should be #HHs (7349500.0)" do
       @scenario.households_heating_pellet_stove_share = 100 #%
@@ -41,9 +32,9 @@ describe "Households, Space Heating" do
       @scenario.turk_number_of_households_space_heater_crude_oil.value.should be_within(10.0).of(7349500.0)
     end
 
-    it "number of households_space_heater_heatpump_air_water_network_gas should be #HHs/(capacity/10kW) (23647.0)" do
-      @scenario.households_heating_gas_fired_heat_pump_share = 100 #%
-      @scenario.turk_number_of_households_space_heater_heatpump_air_water_network_gas.value.should be_within(10.0).of(23647.0)
+    it "number of households_space_heater_heatpump_air_water_electricity should be #HHs (7349500.0)" do
+      @scenario.households_space_heater_heatpump_air_water_electricity_share  = 100 #%
+      @scenario.turk_number_of_units_of_households_space_heater_heatpump_air_water_electricity.value.should be_within(10.0).of(7349500.0)
     end
 
     it "number of households_space_heater_micro_chp_network_gas should be #HHs/2 = 3674750" do
@@ -60,6 +51,17 @@ describe "Households, Space Heating" do
       @scenario.households_heating_gas_fired_heater_share = 100 #%
       @scenario.turk_number_of_households_space_heater_network_gas.value.should be_within(10.0).of(7349500.0)
     end
+
+
+
+
+    # For every hot water technology, the turk_number_of_units should go to #HHs when slider = 100%
+    it "number of households_water_heater_heatpump_air_water_electricity should be #HHs (7349500.0)" do
+      @scenario.households_water_heater_heatpump_air_water_electricity_share  = 100 #%
+      @scenario.turk_number_of_units_of_households_water_heater_heatpump_air_water_electricity.value.should be_within(10.0).of(7349500.0)
+    end
+
+
 
     # The overall employment should change with different sliders
     it "should decrease employment when the Electric heater is pulled to max" do
