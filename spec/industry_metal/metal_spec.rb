2545c5de15b1e822c81b2ed8e65385a980f9ba68
Chael Kruip <chael.kruip@quintel.com>
2012-10-16 09:23:51 +0200

    Improved spec for metal: now fails on the correct test: MdH expects Hisarna furnace to be more efficient than BAT.

--- spec/industry_metal/metal_spec.rb
+++ spec/industry_metal/metal_spec.rb
@@ -9,30 +9,59 @@ describe "Standard scenario: Metal production" do
 
   describe "Steel share group:" do
     
-    it "Setting industry_steel_blastfurnace_bat_share = 100 % should increase demand" do
+    it "Using industry_steel_hisarna instead of BAT should decrease demand" do
+
+      # Initialize to 100 % current
+      @scenario.industry_steel_blastfurnace_current_share = 100 #% 
+      @scenario.industry_steel_blastfurnace_bat_share = 0 #%
+      @scenario.industry_steel_hisarna_share = 0 #%
+      @scenario.industry_steel_electricfurnace_share = 0 #%
+      pp "CURRENT: dashboard_energy_demand_primary_of_final: " + @scenario.dashboard_energy_demand_primary_of_final.future.to_s
+
+      # Now put bat to 100 %      
       @scenario.industry_steel_blastfurnace_current_share = 0 #% 
       @scenario.industry_steel_blastfurnace_bat_share = 100 #%
       @scenario.industry_steel_hisarna_share = 0 #%
-      @scenario.industry_steel_electricfurnace_share = 0 #%
+      pp "BAT: dashboard_energy_demand_primary_of_final: " + @scenario.dashboard_energy_demand_primary_of_final.future.to_s
 
-      @scenario.dashboard_energy_demand_primary_of_final.should increase
-    end
+      @scenario.dashboard_energy_demand_primary_of_final.should decrease
 
-    it "When putting industry_steel_hisarna to 5 % should decrease total energy demand" do
+      puts "When putting industry_steel_hisarna to 1 % should decrease total energy demand"
+
       # Putting the Smeltoven to 100%
       @scenario.industry_steel_blastfurnace_current_share = 0 #% 
-      @scenario.industry_steel_blastfurnace_bat_share = 95 #%
-      @scenario.industry_steel_hisarna_share = 5 #%
+      @scenario.industry_steel_blastfurnace_bat_share = 99 #%
+      @scenario.industry_steel_hisarna_share = 1 #%
+      pp "HISARNA: dashboard_energy_demand_primary_of_final: " + @scenario.dashboard_energy_demand_primary_of_final.future.to_s
 
       # The Cyclone furnace is more efficient, so we would expect the total energy to decrease
       @scenario.dashboard_energy_demand_primary_of_final.should decrease
-
-      # What is the relative change of demand from the 'transformation' options for bat and hisarna?
-      pp "energy_steel_blastfurnace_current_transformation_demand: " + @scenario.energy_steel_blastfurnace_current_transformation_demand.future.to_s
-      # What is the 
-      pp "energy_steel_hisarna_transformation_demand: " + @scenario.energy_steel_hisarna_transformation_demand.future.to_s
-      # What is the 
-      pp "industry_useful_demand_useable_heat_demand: " + @scenario.industry_useful_demand_useable_heat_demand.future.to_s
     end
+
+    it "should decrease the load of the backup burner when putting the hisarna to 1%" do
+
+      # Now put bat to 100 %      
+      @scenario.industry_steel_blastfurnace_current_share = 0 #% 
+      @scenario.industry_steel_blastfurnace_bat_share = 100 #%
+      @scenario.industry_steel_hisarna_share = 0 #%
+      pp "BAT to 100% dashboard_energy_demand_primary_of_final: " + @scenario.dashboard_energy_demand_primary_of_final.future.to_s
+
+      # Putting the Smeltoven to 100%
+      @scenario.industry_steel_blastfurnace_current_share = 0 #% 
+      @scenario.industry_steel_blastfurnace_bat_share = 99 #%
+      @scenario.industry_steel_hisarna_share = 1 #%
+
+      @scenario.energy_heat_network_backup_heater_gas_power_fuelmix_demand.should decrease
+    end
+
+    it "should not increase dumped heat" do
+      @scenario.energy_heat_remainder_from_heat_network_unused_steam_hot_water_demand.value.should == 0
+    end
+
+    # it "should give some output" do 
+    #   pp "energy_steel_blastfurnace_current_transformation_demand: " + @scenario.energy_steel_blastfurnace_current_transformation_demand.future.to_s
+    #   pp "energy_steel_hisarna_transformation_demand: " + @scenario.energy_steel_hisarna_transformation_demand.future.to_s
+    #   pp "industry_useful_demand_useable_heat_demand: " + @scenario.industry_useful_demand_useable_heat_demand.future.to_s
+    # end
   end
 end
