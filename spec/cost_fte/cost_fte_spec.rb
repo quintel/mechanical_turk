b2730548d248e5df0ed32385438d34665f86223e
Chael Kruip <chael.kruip@quintel.com>
2013-03-26 11:54:14 +0100

    Made cost_of_fte specs pending (the cost_of_fte dashboard item is not used anyway)

--- spec/cost_fte/cost_fte_spec.rb
+++ spec/cost_fte/cost_fte_spec.rb
@@ -9,26 +9,26 @@ describe "Cost of FTE dashboard item" do
 
   context "If employment delta is zero, dashboard should return zero" do
 
-    it "should not give an error if present scenario == future scenario" do
+    xit "should not give an error if present scenario == future scenario" do
       expect(@scenario.dashboard_cost_fte).to not_change
     end
   end
 
   context "If a technology is cheap but is job-intensive..." do
 
-    it "should increase when using more solar panels" do
-      @scenario.households_market_penetration_solar_panels = 100 #%
+    xit "should decrease when using more wood-pellet stoves" do
+      @scenario.households_heating_pellet_stove_share = 100 #%
       # # binding.pry
-      expect(@scenario.dashboard_cost_fte).to increase
+      expect(@scenario.dashboard_cost_fte).to decrease
 
     end
   end
 
   context "If a technology is expensive but not job-intensive..." do
 
-    it "should decrease when using more micro CHPs" do
+    xit "should increase when using more micro CHPs" do
       @scenario.households_heating_micro_chp_share = 50 #%
-      expect(@scenario.dashboard_cost_fte).to decrease
+      expect(@scenario.dashboard_cost_fte).to increase
     end
   end
 
