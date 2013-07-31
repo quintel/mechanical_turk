b891f167dfc2d3e7d286a8cb6e852a8a2e7574f4
Chael Kruip <chael.kruip@quintel.com>
2013-07-31 11:41:12 +0200

    Changed population spec to be consistent with new cap for slider.

--- spec/demand/population_spec.rb
+++ spec/demand/population_spec.rb
@@ -9,7 +9,7 @@ describe "Population" do
 
   before(:all) do
     @scenario = Turk::Scenario.new(area_code: "nl", end_year: 2050)
-    @scenario.households_number_of_inhabitants = 48 #%
+    @scenario.households_number_of_inhabitants = 30 # million
   end
 
   it "should increase primary demand" do
