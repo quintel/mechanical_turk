426126781bdb7c7150f5aa7c9a0d06dde5669244
Chael Kruip <chael.kruip@quintel.com>
2014-11-03 12:24:51 +0100

    Fixed and activated Merit Order specs.

--- spec/merit_order/merit_order_spec.rb
+++ spec/merit_order/merit_order_spec.rb
@@ -1,13 +1,14 @@
-# Testing basic behavior of the Merit Order calculation
+# Spec tests to check if Step I of the Merit Order project with EnergieNederland behaves correctly
 require 'spec_helper'
 
-describe "merit order module" do
+describe "Merit Order" do
+
 
   before(:each) do
     @scenario = Turk::Scenario.new(area_code: "nl", end_year: 2050)
   end
 
-  describe "for the start scenario" do
+  context "Without using the merit order" do
 
     it "the full load hours of the cheapest five power plants should be greater than zero and less than 8760" do
       expect(@scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table.value).to be_within(4379).of(4380)
@@ -18,41 +19,130 @@ describe "merit order module" do
     end
   end
 
-  context "when removing power plants" do
+  # Tests that the merit order calculation injects/overwrites the newly calculated demands
+  # back into the model.
+  context "Basic behavior" do
 
-    xit "the full load hours of a power plant should become zero if the number of units becomes zero" do
-      @scenario.number_of_energy_power_combined_cycle_network_gas = 0
-      expect(@scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table.future).to be == 0.0
+    it "Enabling/Disabling merit order should change the `use_merit_order_demands` flag" do
+      # Test that enabling/disabling merit order works.
 
-      @scenario.number_of_energy_power_nuclear_gen3_uranium_oxide = 0
-      expect(@scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table.value).to be == 0.0
+      # by default turned off:
+      @scenario.use_merit_order_demands_enabled.future.should == 0
+
+      # turning it on
+      @scenario.settings_enable_merit_order = 1
+
+      # only inject merit order from future:
+      @scenario.use_merit_order_demands_enabled.future.should == 1
+    end
+
+    it "Enabling/Disabling merit order should change total electricity production" do
+      # Test that enabling/disabling merit order works.
+
+      # by default turned off:
+      @scenario.total_electricity_produced.future_decrease.should == 0
+
+      # turning it on
+      @scenario.settings_enable_merit_order = 1
+
+      # Attention: total_electricity_produced changes because FLH calculated by MO 
+      # are different than those from research
+      @scenario.total_electricity_produced.should change
+    end
+
+    it "Enabling merit order should affect demand of (dispatchable) coal plants" do
+      # turning it on
+      @scenario.settings_enable_merit_order = 1
 
       @scenario.number_of_energy_power_ultra_supercritical_coal = 0
-      expect(@scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table.value).to be == 0.0
+      @scenario.energy_power_ultra_supercritical_coal_demand.value.should == 0
+
+      @scenario.number_of_energy_power_ultra_supercritical_coal = 14
+      @scenario.energy_power_ultra_supercritical_coal_demand.should increase
+    end
+
+    # Import is possible alone if total installed capacity of dispatchables is too small
+    # This is not the case in the start scenarion though.
+    it "merit_order enabled should bring import back to zero" do
+      @scenario.settings_enable_merit_order = 1
+
+      # Turning on merit order should reduce import to zero
+      expect(@scenario.costs_of_imported_electricity.value).to be_within(100.0).of(0.0)
+    end
+
+    it "merit_order enabled should bring export back to zero" do
+      @scenario.settings_enable_merit_order = 1
+
+      # Turning on merit order should reduce export to zero
+      expect(@scenario.primary_demand_of_exported_electricity.value).to be_within(100.0).of(0.0)
     end
   end
+  
+  describe 'Advanced behavior: second-order checks' do
 
-  context "when adding wind power" do
-
-    xit "should lower full load hours of central production power plants" do
-      @scenario.number_of_energy_power_wind_turbine_offshore = 4000
-      expect(@scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table).to decrease
-      expect(@scenario.merit_order_nuclear_iii_full_load_hours_in_merit_order_table).to decrease
-      expect(@scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table).to decrease
-    end
-  end
-
-  context "when fuel costs change" do
-
-    xit "should increase full load hours of gas power plants when natural gas price decreases" do
-      @scenario.costs_gas = -50
-      expect(@scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table).to increase
+    # With the settings_enable_merit_order enabled
+    before(:each) do
+      @scenario = Turk::Scenario.new(area_code: "nl", end_year: 2050, inputs: {
+        settings_enable_merit_order: 1
+      })
     end
 
-    xit "should decrease full load hours of coal power plants when coal price increases" do
-        @scenario.costs_coal = 300
+    context "With merit order enabled" do
+
+      it "adding a lot of energy_power_wind_turbine_offshore should lower full load hours of central production power plants" do
+        @scenario.number_of_energy_power_wind_turbine_offshore = 50000
+        expect(@scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table).to decrease
+        expect(@scenario.merit_order_nuclear_ii_full_load_hours_in_merit_order_table).to decrease
         expect(@scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table).to decrease
-    end
-  end
+      end
+   
+      it "increasing number_of_energy_power_wind_turbine_inland with 1 should not change total produced electricity" do
+        @scenario.number_of_energy_power_wind_turbine_inland = 477
 
+        # ONLY true if some dispatchables still have FLH > 0
+        @scenario.total_electricity_produced.increase.should be_within(100.0).of(0.0)
+
+        # should not give export
+        expect(@scenario.primary_demand_of_exported_electricity.value).to be_within(100.0).of(0.0)
+      end
+
+      it "decreasing number_of_energy_power_wind_turbine_inland with 1 should not change total produced electricity" do
+        @scenario.number_of_energy_power_wind_turbine_inland = 475
+        @scenario.total_electricity_produced.increase.should be_within(100.0).of(0.0)
+
+        # should not give export
+        expect(@scenario.primary_demand_of_exported_electricity.value).to be_within(100.0).of(0.0)
+      end
+    end
+
+    context "Changing fuel costs" do
+
+      it "decreasing gas price should increase full load hours of gas power plants" do
+        @scenario.costs_gas = -50
+        expect(@scenario.merit_order_gas_ccgt_full_load_hours_in_merit_order_table).to increase
+      end
+
+      it "increasing the coal price should decrease full load hours of coal power plants" do
+          @scenario.costs_coal = 300
+          expect(@scenario.merit_order_coal_pwd_full_load_hours_in_merit_order_table).to decrease
+      end
+ 
+      it "increasing the cost of coal to 1000% should decrease the FLHs of coal plants to 0" do
+        # Increasing coal cost by a lot...
+        @scenario.costs_coal = 1000
+
+        # does descrease the demand of coal plants to zero
+        @scenario.energy_power_ultra_supercritical_coal_demand.future.should == 0
+      end
+
+      it "merit_order disabled leaves demands of coal plants as they are" do
+        @scenario.settings_enable_merit_order = 0
+
+        @scenario.costs_coal = 1000
+        @scenario.energy_power_ultra_supercritical_coal_demand.future_increase.should == 0
+
+      end
+    end
+
+  end
 end
