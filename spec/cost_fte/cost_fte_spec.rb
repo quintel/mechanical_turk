726a66ce433bb8591101c5df2a087a4258605904
Dorine van der Vlies <dorine.vandervlies@quintel.com>
2017-09-12 14:14:34 +0200

    changed gquery and reasoning behind tests

--- spec/cost_fte/cost_fte_spec.rb
+++ spec/cost_fte/cost_fte_spec.rb
@@ -1,7 +1,7 @@
 # This Rspec test verifies basic behavior of the Cost of FTE dashaboard item.
 require 'spec_helper'
 
-describe "Cost of FTE dashboard item" do
+describe "Cost of employment dashboard item" do
 
   before(:each) do
     @scenario = Turk::Scenario.new(area_code: "nl", end_year: 2050, autobalance: true)
@@ -9,26 +9,26 @@ describe "Cost of FTE dashboard item" do
 
   context "If employment delta is zero, dashboard should return zero" do
 
-    xit "should not give an error if present scenario == future scenario" do
-      expect(@scenario.dashboard_cost_fte).to not_change
+    it "should not give an error if present scenario == future scenario" do
+      expect(@scenario.dashboard_employment).to not_change
     end
   end
 
   context "If a technology is cheap but is job-intensive..." do
 
-    xit "should decrease when using more wood-pellet stoves" do
+    it "should increase when using more wood-pellet stoves" do
       @scenario.households_space_heater_wood_pellets_share = 100 #%
       # # binding.pry
-      expect(@scenario.dashboard_cost_fte).to decrease
+      expect(@scenario.dashboard_employment).to increase
 
     end
   end
 
   context "If a technology is expensive but not job-intensive..." do
 
-    xit "should increase when using more micro CHPs" do
+    it "should decrease when using more micro CHPs" do
       @scenario.households_space_heater_micro_chp_network_gas_share = 50 #%
-      expect(@scenario.dashboard_cost_fte).to increase
+      expect(@scenario.dashboard_employment).to decrease
     end
   end
 
