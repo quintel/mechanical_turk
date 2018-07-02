5a227b7b1ac1fa9ccfe7dc4b5a74ab393603a4e4
Mart Lubben <mart.lubben@quintel.com>
2018-07-02 12:11:49 +0200

    Text edit in merit_order

--- spec/merit_order/merit_order_spec.rb
+++ spec/merit_order/merit_order_spec.rb
@@ -65,7 +65,7 @@ describe "Merit Order" do
 
     # Import is possible alone if total installed capacity of dispatchables is too small
     # This is not the case in the start scenarion though.
-    it "merit_order enabled and a high import electricity price should bring import back to zero" do
+    it "merit_order enabled and an import price of 500 euro/MWh, which makes import the most expensive based on marginal costs, should bring import back to zero" do
       @scenario.costs_imported_electricity = 500.0
       @scenario.settings_enable_merit_order = 1
 
