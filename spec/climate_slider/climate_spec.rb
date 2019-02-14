6329af51067827fefea59362a86387a400a84a4d
Chael Kruip <chael.kruip@quintel.com>
2019-02-14 18:54:32 +0100

    Fixed climate sliders specs

--- spec/climate_slider/climate_spec.rb
+++ spec/climate_slider/climate_spec.rb
@@ -10,62 +10,58 @@ describe "Testing climate slider" do
   context "Testing impact on heat demand of residences" do
 
     it "it should decrease heat demand if climate slider is pulled to max" do
-      demand_before_update = @scenario.households_new_houses_useful_demand_for_heating.future
+      demand_before_update = @scenario.households_apartments_useful_demand_for_space_heating.future
       #pp demand_before_update
 
       @scenario.households_climate_influence = 5.0 # degrees centigrade
 
-      demand_after_update = @scenario.households_new_houses_useful_demand_for_heating.future
+      demand_after_update = @scenario.households_apartments_useful_demand_for_space_heating.future
       #pp demand_after_update
       #pp demand_after_update / demand_before_update
 
-      @scenario.households_new_houses_useful_demand_for_heating.should decrease
-      @scenario.households_old_houses_useful_demand_for_heating.should decrease
+      @scenario.households_apartments_useful_demand_for_space_heating.should decrease
       @scenario.buildings_useful_demand_for_space_heating.should decrease
     end
 
     it "it should increase heat demand if climate slider is pulled to min" do
-      demand_before_update = @scenario.households_new_houses_useful_demand_for_heating.future
+      demand_before_update = @scenario.households_apartments_useful_demand_for_space_heating.future
       #pp demand_before_update
 
       @scenario.households_climate_influence = -5.0 # degrees centigrade
 
-      demand_after_update = @scenario.households_new_houses_useful_demand_for_heating.future
+      demand_after_update = @scenario.households_apartments_useful_demand_for_space_heating.future
       #pp demand_after_update
       #pp demand_after_update / demand_before_update
 
-      @scenario.households_new_houses_useful_demand_for_heating.should increase
-      @scenario.households_old_houses_useful_demand_for_heating.should increase
+      @scenario.households_apartments_useful_demand_for_space_heating.should increase
       @scenario.buildings_useful_demand_for_space_heating.should increase
     end
 
     it "it should increase cooling demand if climate slider is pulled to max" do
-      demand_before_update = @scenario.households_new_houses_useful_demand_for_cooling.future
+      demand_before_update = @scenario.households_useful_demand_for_cooling.future
       #pp demand_before_update
 
       @scenario.households_climate_influence = 5.0 # degrees centigrade
 
-      demand_after_update = @scenario.households_new_houses_useful_demand_for_cooling.future
+      demand_after_update = @scenario.households_useful_demand_for_cooling.future
       #pp demand_after_update
       #pp demand_after_update / demand_before_update
 
-      @scenario.households_new_houses_useful_demand_for_cooling.should increase
-      @scenario.households_old_houses_useful_demand_for_cooling.should increase
+      @scenario.households_useful_demand_for_cooling.should increase
       @scenario.buildings_useful_demand_cooling.should increase
     end
 
     it "it should decrease cooling demand if climate slider is pulled to min" do
-      demand_before_update =  @scenario.households_new_houses_useful_demand_for_cooling.future
+      demand_before_update =  @scenario.households_useful_demand_for_cooling.future
       #pp demand_before_update
 
       @scenario.households_climate_influence = -5.0 # degrees centigrade
 
-      demand_after_update = @scenario.households_new_houses_useful_demand_for_cooling.future
+      demand_after_update = @scenario.households_useful_demand_for_cooling.future
       #pp demand_after_update
       #pp demand_after_update / demand_before_update
 
-      @scenario.households_new_houses_useful_demand_for_cooling.should decrease
-      @scenario.households_old_houses_useful_demand_for_cooling.should decrease
+      @scenario.households_useful_demand_for_cooling.should decrease
       @scenario.buildings_useful_demand_cooling.should decrease
     end
   end
