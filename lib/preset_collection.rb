5ff4129f1a22a581dd22168f98be796dacae5f33
Nora Schinkel <ncschinkel@gmail.com>
2021-05-21 13:33:14 +0200

    New ways to start a PresetCollection: from multiple keys, or with all scenarios

--- lib/preset_collection.rb
+++ lib/preset_collection.rb
@@ -9,7 +9,8 @@ module Turk
     end
   end
 
-  # Something about this class
+  # A collection of Presets to be looping tests over. Specific test-groups of
+  # scenarios can be added to PRESET_SCENARIOS.
   class PresetCollection
     include Enumerable
 
@@ -25,14 +26,57 @@ module Turk
       @presets = presets
     end
 
-    def self.from_key(key)
-      raise UnknownPresetError.new(key) unless PRESET_SCENARIOS.key? key
-
-      PresetCollection.new(PRESET_SCENARIOS[key].map { |id| Turk::Preset.new(id) })
-    end
-
     def each(&block)
       @presets.each(&block)
+    end
+
+    class << self
+      # Public: Creates and returns a new collection of Presets based on the
+      # scenarios specified by the given key in PRESET_SCENARIOS.
+      #
+      # key,      A key for a scenario test-groups, this key should be present in
+      #           PRESET_SCENARIOS.
+      #
+      # Returns a PresetCollection
+      def from_key(key)
+        ensure_valid_key(key)
+
+        PresetCollection.new(PRESET_SCENARIOS[key].map { |id| Turk::Preset.new(id) })
+      end
+
+      # Public: Creates and returns a new collection of Presets based on the
+      # scenarios specified by the given keys in PRESET_SCENARIOS.
+      #
+      # *keys,    One or multiple keys for scenario test-groups, these keys should
+      #           be present in PRESET_SCENARIOS.
+      #
+      # Returns a PresetCollection
+      def from_keys(*keys)
+        scenarios = keys.flat_map do |key|
+          ensure_valid_key(key)
+          PRESET_SCENARIOS[key]
+        end
+
+        PresetCollection.new(scenarios.map { |id| Turk::Preset.new(id) })
+      end
+
+      # Public: Creates and returns a new collection of Presets containing all
+      # scenarios specified in PRESET_SCENARIOS.
+      #
+      # Returns a PresetCollection
+      def all
+        PresetCollection.new(unique_preset_scenarios.map { |id| Turk::Preset.new(id) })
+      end
+
+      private
+
+      def ensure_valid_key(key)
+        raise UnknownPresetError.new(key) unless PRESET_SCENARIOS.key? key
+      end
+
+      def unique_preset_scenarios
+        PRESET_SCENARIOS.flat_map { |_, scenarios| scenarios }.uniq
+      end
     end
   end
 end
