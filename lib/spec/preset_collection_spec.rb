eced228fb920e8662a3b1ee0fbfab9c5768bde91
Nora Schinkel <ncschinkel@gmail.com>
2021-05-21 13:33:14 +0200

    New ways to start a PresetCollection: from multiple keys, or with all scenarios

--- lib/spec/preset_collection_spec.rb
+++ lib/spec/preset_collection_spec.rb
@@ -7,10 +7,11 @@ describe Turk::PresetCollection do
     load 'webmock_stubs_v3.rb'
   end
 
+  let(:valid_key) { :ii3050 }
+
   describe '.from_key' do
-    context 'with existing key :basic' do
-      let(:key) { :ii3050 }
-      subject { described_class.from_key(key) }
+    context 'with existing key :ii3050' do
+      subject { described_class.from_key(valid_key) }
 
       it 'is a collection' do
         expect(subject).to be_a Turk::PresetCollection
@@ -21,7 +22,7 @@ describe Turk::PresetCollection do
       end
 
       it 'has three scenarios in its collection' do
-        expect(subject.presets.length).to eq described_class::PRESET_SCENARIOS[key].length
+        expect(subject.presets.length).to eq described_class::PRESET_SCENARIOS[valid_key].length
       end
     end
 
@@ -32,16 +33,51 @@ describe Turk::PresetCollection do
     end
   end
 
+  describe '.from_keys' do
+    context 'with existing keys :ii3050 and :ii3050' do
+      subject { described_class.from_keys(valid_key, valid_key) }
+
+      it 'is a collection' do
+        expect(subject).to be_a Turk::PresetCollection
+      end
+
+      it 'contains Presets' do
+        expect(subject.presets[0]).to be_a Turk::Preset
+      end
+
+      it 'has three scenarios in its collection' do
+        expect(subject.presets.length).to eq(
+          2 * described_class::PRESET_SCENARIOS[valid_key].length
+        )
+      end
+    end
+
+    context 'with non existent key' do
+      it 'raises an error' do
+        expect { described_class.from_keys(:cupcake_mix, valid_key) }.to raise_error(
+          Turk::UnknownPresetError
+        )
+      end
+    end
+  end
+
+  describe '.all' do
+    it 'contains all scenarios' do
+      expect(described_class.all.presets.length).to eq(
+        described_class::PRESET_SCENARIOS.values.flatten.uniq.length
+      )
+    end
+  end
+
   describe '#each' do
-    let(:key) { :ii3050 }
-    let(:collection) { described_class.from_key(key) }
+    let(:collection) { described_class.from_key(valid_key) }
 
     it 'behaves like each' do
       counter = 0
       collection.each do |_|
         counter += 1
       end
-      expect(counter).to eq described_class::PRESET_SCENARIOS[key].length
+      expect(counter).to eq described_class::PRESET_SCENARIOS[valid_key].length
     end
 
     it 'loops through all the scenarios' do
