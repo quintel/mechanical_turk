b749a02d099a153f85cd1d33787e4a853c1543c0
Michiel den Haan <michiel.denhaan@quintel.com>
2021-06-03 17:10:52 +0200

    Add hydrogen spec to check whether annual demand and supply match
    Closes #153

--- spec/demand/primary_demand_spec.rb
+++ spec/demand/primary_demand_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'Primary Demand' do
-  Turk::PresetCollection.from_key(:ii3050).each do |scenario|
+  Turk::PresetCollection.from_keys(:ii3050, :nvdt).each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
       # Test whether application group covers all final demand and exports
       it 'primary demand of the application group nodes should always equal primary demand of the
@@ -23,6 +23,15 @@ describe 'Primary Demand' do
           scenario.total_primary_demand
         ).to softly_equal(
           scenario.primary_demand_of_final_demand_export_curtailment_and_refinery_gas
+        )
+      end
+
+      it 'the primary demand per sector chart should always equal total primary demand
+        of final_demand_group, energy_export group and curtailment.' do
+        expect(
+          scenario.turk_queries_primary_demand_per_sector
+        ).to softly_equal(
+          scenario.primary_demand_of_final_demand_export_and_curtailment
         )
       end
     end
