c2a417281b99593ef90625f51fa2c40724b77b66
Michiel den Haan <michiel.denhaan@quintel.com>
2021-07-08 17:48:30 +0200

    Add more scenario groups to hydrogen, primary demand specs

--- spec/demand/hydrogen_spec.rb
+++ spec/demand/hydrogen_spec.rb
@@ -3,7 +3,7 @@
 require 'spec_helper'
 
 describe 'Hydrogen' do
-  Turk::PresetCollection.from_keys(:ii3050, :nvdt).each do |scenario|
+  Turk::PresetCollection.from_keys(:ii3050, :nvdt, :scenario_collection, :merit_off).each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
       it 'Annual demand and supply of hydrogen should match' do
         expect(
