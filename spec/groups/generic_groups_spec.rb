69f43efc63885f3799d189accc0cbe2f60fe54fe
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2023-02-23 16:30:08 +0100

    Add households to  generic spec of tests whether groups align

--- spec/groups/generic_groups_spec.rb
+++ spec/groups/generic_groups_spec.rb
@@ -4,7 +4,9 @@ require 'spec_helper'
 
 describe 'Generic groups tests' do
   Turk::PresetCollection.from_keys(:ii3050, :nvdt, :scenario_collection, :merit_off).each do |scenario|
+    
     context "with scenario #{scenario.original_scenario_id}" do
+      
       # Test whether total final demand in industry edge groups equals total final demand in industry node group
       it 'Sum of final demand edge groups should match sum of final demand node group in industry' do
         skip ('Edge groups include ambient heat in chemical other (heatpump & recompression), node group does not include this, TODO address difference')
@@ -12,6 +14,15 @@ describe 'Generic groups tests' do
           scenario.turk_final_demand_in_industry_edge_groups
         ).to softly_equal(
           scenario.turk_final_demand_in_industry_node_groups
+        )
+      end
+
+      # Test whether total final demand in households edge groups equals total final demand in households node group
+      it 'Sum of final demand edge groups should match sum of final demand node group in households' do
+        expect(
+          scenario.turk_final_demand_in_households_edge_groups
+        ).to softly_equal(
+          scenario.turk_final_demand_in_households_node_groups
         )
       end
 
