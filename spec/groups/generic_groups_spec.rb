7b7bf24e6a27ce64ce53511f1e693a8d96f8df99
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2023-02-23 11:26:39 +0100

    Add generic spec to tests whether groups align

---
+++ spec/groups/generic_groups_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Generic groups tests' do
+  Turk::PresetCollection.from_keys(:ii3050, :nvdt, :scenario_collection, :merit_off).each do |scenario|
+    context "with scenario #{scenario.original_scenario_id}" do
+      # Test whether total final demand in industry edge groups equals total final demand in industry node group
+      it 'Sum of final demand edge groups should match sum of final demand node group in industry' do
+        skip ('Edge groups include ambient heat in chemical other (heatpump & recompression), node group does not include this, TODO address difference')
+        expect(
+          scenario.turk_final_demand_in_industry_edge_groups
+        ).to softly_equal(
+          scenario.turk_final_demand_in_industry_node_groups
+        )
+      end
+
+    end
+  end
+end
