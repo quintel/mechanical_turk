e23eb6b89fdae1b0f09f9d217e8e7b0afa9ece03
Kas Kranenburg <kas.kranenburg@quintel.com>
2025-01-09 15:08:42 +0100

    Adding spec of mekko-ht-heat-network

--- spec/mekko/mekko_supply_demand_spec.rb
+++ spec/mekko/mekko_supply_demand_spec.rb
@@ -1,42 +1,22 @@
 require 'spec_helper'
 
-MEKKO_AND_QUERIES = {
-    'mekko_of_collective_heat_ht' => [:turk_mekko_of_collective_heat_ht_demand, :turk_mekko_of_collective_heat_ht_supply, :turk_mekko_of_collective_heat_ht_network_total_demand, :turk_mekko_of_collective_heat_ht_network_total_supply],
-    'mekko_of_collective_heat_mt' => [:turk_mekko_of_collective_heat_mt_demand, :turk_mekko_of_collective_heat_mt_supply, :turk_mekko_of_collective_heat_mt_network_total_demand, :turk_mekko_of_collective_heat_mt_network_total_supply],
-    'mekko_of_collective_heat_lt' => [:turk_mekko_of_collective_heat_lt_demand, :turk_mekko_of_collective_heat_lt_supply, :turk_mekko_of_collective_heat_lt_network_total_demand, :turk_mekko_of_collective_heat_lt_network_total_supply]
-  }
-
-describe 'Mekko balancing' do
-  Turk::PresetCollection.from_keys(:ii3050v2,:ii3050,:scenario_collection,:merit_off).each do |scenario|
+RSpec.describe 'Mekko balancing' do
+  Turk::PresetCollection.from_keys(:ii3050v2).each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
-      MEKKO_AND_QUERIES.each do |key, (demand_mekko, supply_mekko, demand_network, supply_network)|
-        # Test whether supply equals demand in mekko
-        it "#{key} - supply should match demand in mekko" do
-          expect(
-            scenario.send(demand_mekko)
-          ).to softly_equal(
-            scenario.send(supply_mekko)
-          )
-        end
-
-        # Test whether demand in mekko equals total demand
-        it "#{key} - demand in mekko should match total demand" do
-        expect(
-          scenario.send(demand_mekko)
-        ).to softly_equal(
-          scenario.send(demand_network)
-        )
-        end
-
-        # Test whether supply in mekko equals total supply
-        it "#{key} - supply in mekko should match total supply" do
-          expect(
-            scenario.send(supply_mekko)
-          ).to softly_equal(
-            scenario.send(supply_network)
-          )
-        end
+      it "should result in all input and output flows of mekko_of_collective_heat_ht to be balanced" do
+        expect(scenario.turk_mekko_of_collective_heat_ht_demand).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_supply)
+      end
+    end
+    context "with scenario #{scenario.original_scenario_id}" do
+      it "should result in all the demand of mekko_of_collective_heat_ht to match the total ht heat demand" do
+        expect(scenario.turk_mekko_of_collective_heat_ht_demand).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_network_total_demand)
+      end
+    end
+    context "with scenario #{scenario.original_scenario_id}" do
+      it "should result in all the supply of mekko_of_collective_heat_ht to be match the total ht heat supply" do
+        expect(scenario.turk_mekko_of_collective_heat_ht_supply).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_network_total_supply)
       end
     end
   end
 end
+
