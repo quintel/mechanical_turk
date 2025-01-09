348898495f006123e9eab60a44f9c99434cd9bb3
Kas Kranenburg <kas.kranenburg@quintel.com>
2025-01-09 16:02:15 +0100

    Adding electricity network spec

--- spec/mekko/mekko_supply_demand_spec.rb
+++ spec/mekko/mekko_supply_demand_spec.rb
@@ -4,16 +4,19 @@ RSpec.describe 'Mekko balancing' do
   Turk::PresetCollection.from_keys(:ii3050v2).each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
       it "should result in all input and output flows of mekko_of_collective_heat_ht to be balanced" do
+        skip
         expect(scenario.turk_mekko_of_collective_heat_ht_demand).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_supply)
       end
     end
     context "with scenario #{scenario.original_scenario_id}" do
       it "should result in all the demand of mekko_of_collective_heat_ht to match the total ht heat demand" do
+        skip
         expect(scenario.turk_mekko_of_collective_heat_ht_demand).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_network_total_demand)
       end
     end
     context "with scenario #{scenario.original_scenario_id}" do
       it "should result in all the supply of mekko_of_collective_heat_ht to be match the total ht heat supply" do
+        skip
         expect(scenario.turk_mekko_of_collective_heat_ht_supply).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_network_total_supply)
       end
     end
@@ -52,7 +55,13 @@ RSpec.describe 'Mekko balancing' do
         skip("Mechanical turk #186")
         expect(scenario.turk_mekko_of_collective_heat_lt_supply).to softly_equal(scenario.turk_mekko_of_collective_heat_lt_network_total_supply)
       end
-    end    
+    end
+    context "with scenario #{scenario.original_scenario_id}" do
+      it "should result in all input and output flows of mekko_of_electricity_network to be balanced" do
+        skip("ETSource 2930")
+        expect(scenario.turk_mekko_of_electricity_network_demand).to softly_equal(scenario.turk_mekko_of_electricity_network_supply)
+      end
+    end        
   end
 end
 
