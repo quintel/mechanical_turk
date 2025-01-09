cccb79b218ad13fad69aee6cd60a6ea23bf863b4
Kas Kranenburg <kas.kranenburg@quintel.com>
2025-01-09 16:08:48 +0100

    Skipping heat network ht test due to ETsource issue 3165

--- spec/mekko/mekko_supply_demand_spec.rb
+++ spec/mekko/mekko_supply_demand_spec.rb
@@ -4,19 +4,17 @@ RSpec.describe 'Mekko balancing' do
   Turk::PresetCollection.from_keys(:ii3050v2).each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
       it "should result in all input and output flows of mekko_of_collective_heat_ht to be balanced" do
-        skip
+        skip("ETSource 3165")
         expect(scenario.turk_mekko_of_collective_heat_ht_demand).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_supply)
       end
     end
     context "with scenario #{scenario.original_scenario_id}" do
       it "should result in all the demand of mekko_of_collective_heat_ht to match the total ht heat demand" do
-        skip
         expect(scenario.turk_mekko_of_collective_heat_ht_demand).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_network_total_demand)
       end
     end
     context "with scenario #{scenario.original_scenario_id}" do
       it "should result in all the supply of mekko_of_collective_heat_ht to be match the total ht heat supply" do
-        skip
         expect(scenario.turk_mekko_of_collective_heat_ht_supply).to softly_equal(scenario.turk_mekko_of_collective_heat_ht_network_total_supply)
       end
     end
