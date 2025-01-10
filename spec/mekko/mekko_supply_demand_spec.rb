177ecef1a97cb01ee9269115ca0de2b36cc65ac3
Kas Kranenburg <kas.kranenburg@quintel.com>
2025-01-10 14:52:44 +0100

    Adding mekko spec for network gas

--- spec/mekko/mekko_supply_demand_spec.rb
+++ spec/mekko/mekko_supply_demand_spec.rb
@@ -59,7 +59,24 @@ RSpec.describe 'Mekko balancing' do
         skip("ETSource 2930")
         expect(scenario.turk_mekko_of_electricity_network_demand).to softly_equal(scenario.turk_mekko_of_electricity_network_supply)
       end
-    end        
+    end 
+    context "with scenario #{scenario.original_scenario_id}" do
+      it "should result in all input and output flows of mekko_of_network_gas_network to be balanced" do
+        expect(scenario.turk_mekko_of_network_gas_network_demand).to softly_equal(scenario.turk_mekko_of_network_gas_network_supply)
+      end
+    end
+    context "with scenario #{scenario.original_scenario_id}" do
+      it "should result in all the demand of mekko_of_collective_heat_lt to match the total lt heat demand" do
+        skip("ETSource #3177")
+        expect(scenario.turk_mekko_of_network_gas_network_demand).to softly_equal(scenario.turk_mekko_of_network_gas_network_total_demand)
+      end
+    end
+    context "with scenario #{scenario.original_scenario_id}" do
+      it "should result in all the supply of mekko_of_collective_heat_lt to be match the total lt heat supply" do
+        skip("ETSource #3177")
+        expect(scenario.turk_mekko_of_network_gas_network_supply).to softly_equal(scenario.turk_mekko_of_network_gas_network_total_supply)
+      end     
+    end    
   end
 end
 
