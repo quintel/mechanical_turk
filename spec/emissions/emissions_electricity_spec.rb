43de5ebf1f130d5b6142ca29134c0c5e68560c82
Kas Kranenburg <kas.kranenburg@quintel.com>
2025-10-13 17:24:37 +0200

    Fix bugs signaled by mechanical turk

--- spec/emissions/emissions_electricity_spec.rb
+++ spec/emissions/emissions_electricity_spec.rb
@@ -14,7 +14,8 @@ describe 'Electricity emissions' do
               scenario.lignite_in_co2_of_electricity_production,
               scenario.coal_in_co2_of_electricity_production,
               scenario.waste_in_co2_of_electricity_production,
-              scenario.hydrogen_in_co2_of_electricity_production
+              scenario.hydrogen_in_co2_of_electricity_production,
+              scenario.torrefied_biomass_pellets_in_co2_of_electricity_production
         	  ]
           ).to sum_to_softly_equal(
           	scenario.turk_co2_emissions_of_produced_electricity
