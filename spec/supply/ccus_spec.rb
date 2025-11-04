c75924da2b7aa5cb3bdd9079ddc5b20e46015970
Kyra de Haan <kyra.dehaan@quintel.com>
2025-11-04 15:50:08 +0100

    Update gasification technologies in ccus_spec

--- spec/supply/ccus_spec.rb
+++ spec/supply/ccus_spec.rb
@@ -63,53 +63,42 @@ describe "CCUS" do
           )
         end
 
-        it "should be equal for energy_production_fischer_tropsch_biogenic_waste_ccs" do
-          expect(
-            scenario.turk_energy_production_fischer_tropsch_biogenic_waste_ccs_capture_share
-          ).to softly_equal(
-            scenario.turk_energy_production_fischer_tropsch_biogenic_waste_ccs_capture_rate
-          )
-        end
-
-        it "should be equal for energy_production_fischer_tropsch_dry_biomass_ccs" do
-          expect(
-            scenario.turk_energy_production_fischer_tropsch_dry_biomass_ccs_capture_share
-          ).to softly_equal(
-            scenario.turk_energy_production_fischer_tropsch_dry_biomass_ccs_capture_rate
-          )
-        end
-        it "should be equal for energy_production_fischer_tropsch_biogenic_waste_ccs" do
-          expect(
-            scenario.turk_energy_production_fischer_tropsch_biogenic_waste_ccs_capture_share
-          ).to softly_equal(
-            scenario.turk_energy_production_fischer_tropsch_biogenic_waste_ccs_capture_rate
-          )
-        end
-
-        it "should be equal for energy_production_methanol_synthesis_biogenic_waste_ccs" do
-          expect(
-            scenario.turk_energy_production_methanol_synthesis_biogenic_waste_ccs_capture_share
-          ).to softly_equal(
-            scenario.turk_energy_production_methanol_synthesis_biogenic_waste_ccs_capture_rate
-          )
-        end
-
-        it "should be equal for energy_production_methanol_synthesis_dry_biomass_ccs" do
-          expect(
-            scenario.turk_energy_production_methanol_synthesis_dry_biomass_ccs_capture_share
-          ).to softly_equal(
-            scenario.turk_energy_production_methanol_synthesis_dry_biomass_ccs_capture_rate
-          )
-        end
-
-        it "should be equal for energy_production_methanol_synthesis_non_biogenic_waste_ccs" do
-          expect(
-            scenario.turk_energy_production_methanol_synthesis_non_biogenic_waste_ccs_capture_share
-          ).to softly_equal(
-            scenario.turk_energy_production_methanol_synthesis_non_biogenic_waste_ccs_capture_rate
-          )
-        end
       end
     end
+  end
+
+  # Test CO2 flows in Fischer-Tropsch and methanol synthesis with CCS for
+  # biogenic carriers and non-biogenic carrier routes
+  context "CO2 flows in gasification technologies with CCS" do
+    before do
+      @scenario = Turk::Scenario.new(area_code: "nl2023", end_year: 2050)
+    end
+
+    # Captured biogenic CO2 leads to negative emissions and should be equal to
+    # the (negative) primary CO2 emissions
+    it "captured CO2 for biogenic carriers should be equal to primary CO2 emissions" do
+      @scenario.output_of_energy_production_fischer_tropsch = 100.0 #PJ
+      @scenario.energy_production_fischer_tropsch_dry_biomass_ccs_share = 100.0 #%
+
+      expect(
+        @scenario.turk_energy_production_fischer_tropsch_dry_biomass_ccs_captured
+      ).to softly_equal(
+        @scenario.turk_energy_production_fischer_tropsch_dry_biomass_ccs_primary_co2
+      )
+    end
+
+    # CO2 input of process node should be equal to the total CO2 output, consisting of
+    # CO2 in products, captured CO2 and emitted CO2 (from molecule graph)
+    it "total input of CO2 should equal total output of CO2" do
+      @scenario.output_of_energy_production_methanol_synthesis = 100.0 #PJ
+      @scenario.energy_production_methanol_synthesis_non_biogenic_waste_ccs_share = 100.0 #%
+
+      expect(
+        @scenario.turk_energy_production_methanol_synthesis_non_biogenic_waste_ccs_co2_in
+      ).to softly_equal(
+        @scenario.turk_energy_production_methanol_synthesis_non_biogenic_waste_ccs_co2_out
+      )
+    end
+
   end
 end
