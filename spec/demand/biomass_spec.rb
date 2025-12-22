09658815f736640be4d43eeac65120df99b76fe4
aaccensi <andreu.accensi@gmail.com>
2025-12-22 10:07:08 +0100

    Global scenario/query caching test on biomass_spec
    References #204

--- spec/demand/biomass_spec.rb
+++ spec/demand/biomass_spec.rb
@@ -3,37 +3,8 @@
 require 'spec_helper'
 
 RSpec.describe 'Biomass' do
-  Turk::PresetCollection.from_keys(:ii3050v2).each do |scenario|
+  $preset_collection_ii3050v2.each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
-      before(:all) do
-        scenario.track(%w[
-          turk_distribution_dry_biomass
-          turk_demand_dry_biomass
-          turk_distribution_wet_biomass
-          turk_demand_wet_biomass
-          turk_distribution_oily_biomass
-          turk_demand_oily_biomass
-          turk_distribution_biogenic_waste
-          turk_demand_biogenic_waste
-          turk_total_input_in_biomass_sankey
-          turk_total_output_in_biomass_sankey
-          turk_input_of_biogenic_waste_in_biomass_sankey
-          turk_output_of_biogenic_waste_in_biomass_sankey
-          turk_input_of_wet_in_biomass_sankey
-          turk_output_of_wet_in_biomass_sankey
-          turk_input_of_oily_in_biomass_sankey
-          turk_output_of_oily_in_biomass_sankey
-          turk_input_of_dry_in_biomass_sankey
-          turk_output_of_dry_in_biomass_sankey
-          turk_input_of_biogas_in_biomass_sankey
-          turk_output_of_biogas_in_biomass_sankey
-          turk_input_of_greengas_in_biomass_sankey
-          turk_output_of_greengas_in_biomass_sankey
-          turk_input_of_biofuels_in_biomass_sankey
-          turk_output_of_biofuels_in_biomass_sankey
-        ])
-      end
-
       it 'results in the total demand of the dry biomass resources to be equal to the distribution demand' do
         expect(
           scenario.turk_distribution_dry_biomass
