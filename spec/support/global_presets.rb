e49bc1fd944891c91ec4b936d670cdd73f864506
Mathijs Bijkerk <mathijs.bijkerk@quintel.com>
2025-12-23 08:58:06 +0100

    Add preset check to Mekko spec

--- spec/support/global_presets.rb
+++ spec/support/global_presets.rb
@@ -30,6 +30,43 @@ RSpec.configure do |config|
         turk_output_of_greengas_in_biomass_sankey
         turk_input_of_biofuels_in_biomass_sankey
         turk_output_of_biofuels_in_biomass_sankey
+        turk_mekko_of_collective_heat_ht_demand
+        turk_mekko_of_collective_heat_ht_supply
+        turk_mekko_of_collective_heat_ht_network_total_demand
+        turk_mekko_of_collective_heat_ht_network_total_supply
+        turk_mekko_of_collective_heat_mt_demand
+        turk_mekko_of_collective_heat_mt_supply
+        turk_mekko_of_collective_heat_mt_network_total_demand
+        turk_mekko_of_collective_heat_mt_network_total_supply
+        turk_mekko_of_collective_heat_lt_demand
+        turk_mekko_of_collective_heat_lt_supply
+        turk_mekko_of_collective_heat_lt_network_total_demand
+        turk_mekko_of_collective_heat_lt_network_total_supply
+        turk_mekko_of_electricity_network_demand
+        turk_mekko_of_electricity_network_supply
+        turk_mekko_of_network_gas_network_demand
+        turk_mekko_of_network_gas_network_supply
+        turk_mekko_of_network_gas_network_total_demand
+        turk_mekko_of_network_gas_network_total_supply
+        turk_agriculture_local_heat_mekko_demand
+        turk_agriculture_local_heat_mekko_supply
+        turk_agriculture_local_heat_mekko_total_demand_and_supply
+        turk_industrial_heat_mekko_demand
+        turk_industrial_heat_mekko_supply
+        turk_industrial_heat_mekko_total_demand_and_supply
+        turk_mekko_of_ammonia_demand_supply_demand
+        turk_mekko_of_ammonia_demand_supply_supply
+        turk_mekko_of_ammonia_demand_supply_total_demand_and_supply
+        turk_mekko_of_co2_demand_supply_demand
+        turk_mekko_of_co2_demand_supply_supply
+        turk_mekko_of_co2_demand_supply_total_demand_and_supply
+        turk_mekko_of_hydrogen_network_demand
+        turk_mekko_of_hydrogen_network_supply
+        turk_mekko_of_hydrogen_network_total_demand
+        turk_mekko_of_hydrogen_network_total_supply
+        turk_mekko_of_kerosene_demand_supply_demand
+        turk_mekko_of_kerosene_demand_supply_supply
+        turk_mekko_of_kerosene_demand_supply_total_demand_and_supply
       ])
     end
 
