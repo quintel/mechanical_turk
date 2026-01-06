b21f98ddd2a9e1d04df8e94d99757669f9f0f174
aaccensi <andreu.accensi@gmail.com>
2026-01-06 11:32:35 +0100

    Move caching at Preset class level and fix lib/spec.
    References #204

--- lib/spec/webmock_stubs_v3.rb
+++ lib/spec/webmock_stubs_v3.rb
@@ -4,6 +4,7 @@
 
 WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[source]=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                   "area_code" => "nl",
                   "end_year" => 2040,
@@ -12,11 +13,12 @@ WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[source]=
                   "use_fce" => false,
                   "user_values" => {},
                   "source" => "Mechanical Turk"
-                }
+                }.to_json
             )
 
 WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[area_code]=nl&scenario[end_year]=2040&scenario[source]=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                   "area_code" => "nl",
                   "end_year" => 2040,
@@ -25,11 +27,12 @@ WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[area_cod
                   "use_fce" => false,
                   "user_values" => {},
                   "source" => "Mechanical Turk"
-                }
+                }.to_json
             )
 
 WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[area_code]=de&scenario[end_year]=2037&scenario[source]=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                  "area_code" => "nl",
                   "end_year" => 2040,
@@ -38,7 +41,7 @@ WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[area_cod
                   "use_fce" => false,
                   "user_values" => {},
                   "source" => "Mechanical Turk"
-                }
+                }.to_json
             )
 
 ##
@@ -48,6 +51,7 @@ WebMock.stub_request(:post, Turk::Connection.base_uri + ".json?scenario[area_cod
 
 WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&gqueries[]=foo&gqueries[]=bar&source=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                "gqueries" => {
                  "foo" => {"present" => 1, "future" => 2, "unit" => "gold"},
@@ -60,15 +64,16 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&gquer
                   "region" => nil,
                   "use_fce" => false,
                   "user_values" => {}
-                  }
-                },
+                  },
                 "errors" => []
+                }.to_json
             )
 
 # Requesting a query called foo
 
 WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&gqueries[]=foo&source=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                "gqueries" => {
                  "foo" => {"present" => 1, "future" => 2, "unit" => "gold"}
@@ -80,9 +85,9 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&gquer
                   "region" => nil,
                   "use_fce" => false,
                   "user_values" => {}
-                  }
-                },
+                  },
                 "errors" => []
+                }.to_json
             )
 
 # Setting the input number_of_energy_power_ultra_supercritical_coal to 10, will 'cause' an increase of foo with 10
@@ -90,6 +95,7 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&gquer
 
 WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&scenario[user_values][number_of_energy_power_ultra_supercritical_coal]=10&gqueries[]=foo&source=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                "gqueries" => {
                  "foo" => {"present" => 11, "future" => 12, "unit" => "gold"}
@@ -101,15 +107,16 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?reset=true&scena
                   "region" => nil,
                   "use_fce" => false,
                   "user_values" => {}
-                  }
-                },
+                  },
                 "errors" => []
+                }.to_json
             )
 
 # User values and queries
 
 WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?gqueries%5B0%5D=foo&reset=true&scenario%5Buser_values%5D%5Bnumber_of_energy_power_ultra_supercritical_coal%5D=10&scenario%5Buser_values%5D%5Bnumber_of_energy_power_ultra_supercritical_ccs_coal%5D=11&source=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                "gqueries" => {
                  "foo" => {"present" => 1, "future" => 2, "unit" => "gold"}
@@ -121,9 +128,9 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?gqueries%5B0%5D=
                   "region" => nil,
                   "use_fce" => false,
                   "user_values" => {}
-                  }
-                },
+                  },
                 "errors" => []
+                }.to_json
             )
 
 # Query that does not exist on ETEngine returns an Error with message
@@ -131,6 +138,7 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?gqueries%5B0%5D=
 
 WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?gqueries[]=unknown&reset=true&source=Mechanical%20Turk"). \
   to_return( :status => 200,
+             :headers => { 'Content-Type' => 'application/json' },
              :body => {
                "gqueries" => {
                  "unknown" => {"errors" => ["Missing gquery"]}
@@ -142,8 +150,7 @@ WebMock.stub_request(:put, Turk::Connection.base_uri + "/1.json?gqueries[]=unkno
                   "region" => nil,
                   "use_fce" => false,
                   "user_values" => {}
-                  }
-                },
+                  },
                 "errors" => []
+                }.to_json
             )
-
