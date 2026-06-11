ed3cc8b83e8e3effbbd2f76752ebf2c914182766
Kyra de Haan <kyra.dehaan@quintel.com>
2026-06-11 16:39:44 +0200

    Update direct emissions spec for 1990 emission validation

--- spec/emissions/direct_emissions.rb
+++ spec/emissions/direct_emissions.rb
@@ -16,6 +16,30 @@ RSpec.describe 'Direct emissions' do
           scenario.direct_emissions_total_ghg_incl_indirect_emissions_lulucf_bunkers
         )
       end
+
+      it "sum of 1990 CO2 emissions per subsector should equal the 1990 CO2 emissions total" do
+        expect(
+          scenario.turk_direct_emissions_co2_1990
+        ).to softly_equal(
+          scenario.direct_emissions_co2_1990
+        )
+      end
+
+      it "sum of 1990 other GHG emissions per subsector should equal the 1990 other GHG emissions total" do
+        expect(
+          scenario.turk_direct_emissions_other_ghg_1990
+        ).to softly_equal(
+          scenario.direct_emissions_other_ghg_1990
+        )
+      end
+
+      it "sum of 1990 CO2 and other GHG emissions should equal the 1990 total GHG emissions" do
+        expect(
+          scenario.turk_direct_emissions_total_ghg_1990
+        ).to softly_equal(
+          scenario.direct_emissions_total_ghg_1990
+        )
+      end
     end
   end
 end
