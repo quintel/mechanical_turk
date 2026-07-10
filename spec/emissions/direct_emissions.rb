176f3c0af66e75baf3c6632e1cfc49bac49ab45c
Kyra de Haan <kyra.dehaan@quintel.com>
2026-07-10 10:29:46 +0200

    Update spec description

--- spec/emissions/direct_emissions.rb
+++ spec/emissions/direct_emissions.rb
@@ -5,9 +5,9 @@ require 'spec_helper'
 RSpec.describe 'Direct emissions' do
   Turk::PresetCollection.all.each do |scenario|
     context "with scenario #{scenario.original_scenario_id}" do
-      # Test correctness of scenario's total GHG emissions. If test fails, this indicated that an
-      # emissions group might be added to a node that is not included in the query that sums total
-      # GHG emissions per sector.
+      # Test correctness of scenario's total GHG emissions. A failing test indicates that the
+      # sector_label might not have been added to a node or that the per sector GHG queries are
+      # not complete.
       it "sum of total GHG emissions of all nodes should equal the sum of total GHG emissions per
         sector" do
         expect(
